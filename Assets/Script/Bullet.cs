4cbd6c91a864f9370e56871581646aa4395cb5ec
leenk01 <alexandre3012@gmail.com>
2020-01-26 18:16:21 -0300

    Pooling Bullets Incomplete

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    
+    [SerializeField]
+    private float moveSpeed = 0.1f;
+
+    private void OnEnable()
+    {
+        Invoke("destroy", 3f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.timeScale);
+    }
+
+    public void setMoveDirection(Vector2 dir) {
+        moveDirection = dir;
+    }
+
+    private void destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
---
+++ Assets/Script/FireBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    [SerializeField]
+    private int bulletsAmount = 1;
+
+    private Vector2 bulletMoveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", 0f, 2f);
+    }
+
+    private void Fire()
+    {
+        for (int i = 0; i < bulletsAmount; i++) {
+            GameObject bul = BulletPool.bulletPoolIntance.getBullet();
+                bul.transform.position = transform.position;
+                bul.transform.rotation = transform.rotation;
+                bul.SetActive(true);
+                bul.GetComponent<Bullet>().setMoveDirection(Vector2.up);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
